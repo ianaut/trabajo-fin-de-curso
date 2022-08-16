016683d101237b0f8909893e56984e7fc676b6f8
ianaut <110979421+ianaut@users.noreply.github.com>
2022-08-16 18:37:46 +0200

    Delete Trabajo fin de curso - copia directory

--- Trabajo fin de curso - copia/includes/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-<div>
-    <!-- <h1 class="titulo">Titulo Pag Web</h1> -->
-    <p><?php echo fecha(); ?></p>
-</div>
-</header>
--- Trabajo fin de curso - copia/includes/scripts.php
+++
@@ -1,3 +0,0 @@
-    <script type="text/javascript" src="functions.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <?php include "includes/functions.php"; ?>
